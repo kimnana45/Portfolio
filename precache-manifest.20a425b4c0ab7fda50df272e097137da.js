fd1b73b615d1370bfa1778bb5278db2bfd4d1a6d
kimnana45 <kimle.lek@gmail.com>
2020-08-19 17:19:17 -0400

    Updates

---
+++ precache-manifest.20a425b4c0ab7fda50df272e097137da.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b0766db46ac9ece8600569b912ad03",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e5f07035dc3f7a3e0c79",
+    "url": "/Portfolio/static/css/main.fc549ce8.chunk.css"
+  },
+  {
+    "revision": "af22a8917c3f321d0b43",
+    "url": "/Portfolio/static/js/2.aaccd3f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.aaccd3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f07035dc3f7a3e0c79",
+    "url": "/Portfolio/static/js/main.ef845386.chunk.js"
+  },
+  {
+    "revision": "efed155d89bc85b81984",
+    "url": "/Portfolio/static/js/runtime-main.88fabfc4.js"
+  },
+  {
+    "revision": "f6625b3b9492b4a4a072f5f64ce67427",
+    "url": "/Portfolio/static/media/IMG_E1205.f6625b3b.png"
+  },
+  {
+    "revision": "5012bdd19e03c777b813918e08dfaa43",
+    "url": "/Portfolio/static/media/TheClassicGenius.5012bdd1.png"
+  },
+  {
+    "revision": "f2499493a7c7ac148b1c58067ab90fa9",
+    "url": "/Portfolio/static/media/WeatherApp.f2499493.png"
+  },
+  {
+    "revision": "f25529e523cb7fd015fb01bf93c35d53",
+    "url": "/Portfolio/static/media/burgerApp.f25529e5.png"
+  },
+  {
+    "revision": "216b616258056ebdfc24e2342cdb6326",
+    "url": "/Portfolio/static/media/employeeTracker.216b6162.png"
+  },
+  {
+    "revision": "6d47297413f9f8887ed574388eb9d06e",
+    "url": "/Portfolio/static/media/noteTaker.6d472974.png"
+  },
+  {
+    "revision": "c1a0a38e0993ba34f6586d9730b798ae",
+    "url": "/Portfolio/static/media/posttosold.c1a0a38e.png"
+  }
+]);
